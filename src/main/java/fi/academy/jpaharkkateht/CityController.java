5fec87b5c5d240833ba9c2f4af670042df943a13
TommiS <tommi.supi@gmail.com>
2020-02-18 11:52:18 +0200

    lisatty kaupungin haku nimella

--- src/main/java/fi/academy/jpaharkkateht/CityController.java
+++ src/main/java/fi/academy/jpaharkkateht/CityController.java
@@ -2,15 +2,27 @@ package fi.academy.jpaharkkateht;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CityController {
-    @Autowired CityRepository cityRepository;
+    @Autowired
+    CityRepository cityRepository;
 
     @GetMapping("/api/kaupungit")
     public Iterable<City> restMetodiGetKaupungit() {
         return cityRepository.findAll();
     }
 
+    @GetMapping("/api/kaupungithaku")
+    public Iterable<City> restMetodiFindKaupunki(@RequestParam(name = "name", required = false) String hakusana) {
+        return cityRepository.findByName(hakusana);
+    }
+
 }
+
+
+
+
+
--- src/main/java/fi/academy/jpaharkkateht/CityRepository.java
+++ src/main/java/fi/academy/jpaharkkateht/CityRepository.java
@@ -3,4 +3,5 @@ package fi.academy.jpaharkkateht;
 import org.springframework.data.repository.CrudRepository;
 
 public interface CityRepository extends CrudRepository<City, Long> {
+    Iterable<City> findByName(String name);
 }
