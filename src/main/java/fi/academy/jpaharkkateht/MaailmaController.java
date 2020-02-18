3075c25b9d3993cab85c9b68a5ee9c47446d6072
TommiS <tommi.supi@gmail.com>
2020-02-18 10:59:43 +0200

    lisatty get by id hakuominaisuus

--- src/main/java/fi/academy/jpaharkkateht/MaailmaController.java
+++ src/main/java/fi/academy/jpaharkkateht/MaailmaController.java
@@ -2,7 +2,10 @@ package fi.academy.jpaharkkateht;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 public class MaailmaController {
@@ -12,6 +15,11 @@ public class MaailmaController {
     @GetMapping("/api/maat")
     public Iterable<Country> restMetodiGetMaat() {
         return countryRepository.findAll();
+    }
+
+    @GetMapping("api/maahaku")
+    public Optional<Country> restMetodiFind(@RequestParam(name="name", required = false) String hakusana) {
+        return countryRepository.findById(hakusana);
     }
 
 
